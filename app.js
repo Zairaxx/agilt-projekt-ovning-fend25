9ce6ac2b85ea383e63f66cb265d724fcba52176c
avdrxxa <damian.raluka.andreea23@gmail.com>
2026-03-11 15:24:59 +0100

    Fixade tillbaka knappen

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ function renderPlayerInfo() {
 <p><b>Country:</b> ${player?.country}</p>
 <p><b>Ranking:</b> ${player?.ranking}</p>
 <br>
-<button onclick="window.location='home.html'">
+<button onclick="window.location='index.html'">
 Back
 </button>
 
